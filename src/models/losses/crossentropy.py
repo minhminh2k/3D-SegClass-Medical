78a3afd44428f3354139642371f51cbcd79df0ea
minhminh2k <minhqd9112003@gmail.com>
2025-03-01 02:33:45 +0900

    Feat: multiple losses from nnUnet

---
+++ src/models/losses/crossentropy.py
@@ -0,0 +1,13 @@
+import torch
+from torch import nn
+
+
+class RobustCrossEntropyLoss(nn.CrossEntropyLoss):
+    """
+    this is just a compatibility layer because my target tensor is float and has an extra dimension
+    """
+    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        if len(target.shape) == len(input.shape):
+            assert target.shape[1] == 1
+            target = target[:, 0]
+        return super().forward(input, target.long())
