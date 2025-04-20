Feat: Softmax output and fix Unified Focal loss

src/models/losses/focal_tversky_3d.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from monai.losses import AsymmetricUnifiedFocalLoss
 from monai.utils import LossReduction
 from monai.networks import one_hot
+import logging
 
 # https://github.com/IvanVassi/FocalTversky3D_pytorch/blob/main/README.md
 # https://github.com/mlyg/unified-focal-loss/blob/main/loss_functions.py
@@ -10,6 +11,8 @@ from monai.networks import one_hot
 class FocalTverskyLoss(nn.Module):
     """A Novel Focal Tversky loss function with improved Attention U-Net for lesion segmentation
     Link: https://arxiv.org/abs/1810.07842
+
+    For binary segmentation -> sigmoid
     """
     def __init__(
         self, 
@@ -285,15 +288,19 @@ class SymmetricUnifiedFocalLoss(nn.Module):
     Michael Yeung, Computerized Medical Imaging and Graphics
     """
     def __init__(
-        self, 
+        self,
+        to_onehot_y: bool = True,
         num_classes: int = 2,
         weight: float = 0.5,
         delta: float = 0.7,
         gamma: float = 0.5,
         epsilon: float = 1e-7,
+        softmax: bool = True,
         reduction: LossReduction | str = LossReduction.MEAN,
     ):
         super().__init__()
+        self.to_onehot_y = to_onehot_y
+        self.softmax = softmax
         self.delta = delta
         self.gamma = gamma
         self.epsilon = epsilon
@@ -320,14 +327,22 @@ class SymmetricUnifiedFocalLoss(nn.Module):
             ValueError: When num_classes
             ValueError: When the number of classes entered does not match the expected number
         """
-        if y_pred.shape != y_true.shape:
-            raise ValueError(f"ground truth has different shape ({y_true.shape}) from input ({y_pred.shape})")
-
         if len(y_pred.shape) != 4 and len(y_pred.shape) != 5:
             raise ValueError(f"input shape must be 4 or 5, but got {y_pred.shape}")
-            
-        if torch.max(y_true) != self.num_classes - 1:
-            raise ValueError(f"Please make sure the number of classes is {self.num_classes-1}")
+        
+        n_pred_ch = y_pred.shape[1]
+        
+        if self.softmax:
+            if n_pred_ch == 1:
+                raise ValueError("single channel prediction, `softmax=True` ignored.")
+            else:
+                y_pred = torch.softmax(y_pred, 1)
+
+        if self.to_onehot_y:
+            if n_pred_ch == 1:
+                raise ValueError("single channel prediction, `to_onehot_y=True` ignored.")
+            else:
+                y_true = one_hot(y_true, num_classes=n_pred_ch)
         
         sy_focal_loss = self.sy_focal_loss(y_pred, y_true)
         sy_focal_tversky_loss = self.sy_focal_tversky_loss(y_pred, y_true)
@@ -359,11 +374,12 @@ class AsymmetricUnifiedFocalLoss(nn.Module):
 
     def __init__(
         self,
-        to_onehot_y: bool = False,
+        to_onehot_y: bool = True,
         num_classes: int = 2,
         weight: float = 0.5,
         gamma: float = 0.5,
         delta: float = 0.7,
+        softmax: bool = True,
         reduction: LossReduction | str = LossReduction.MEAN,
     ):
         """
@@ -390,6 +406,7 @@ class AsymmetricUnifiedFocalLoss(nn.Module):
         self.delta = delta
         self.weight: float = weight
         self.reduction = reduction
+        self.softmax = softmax
         self.asy_focal_loss = AsymmetricFocalLoss(gamma=self.gamma, delta=self.delta)
         self.asy_focal_tversky_loss = AsymmetricFocalTverskyLoss(gamma=self.gamma, delta=self.delta)
 
@@ -410,15 +427,23 @@ class AsymmetricUnifiedFocalLoss(nn.Module):
             ValueError: When num_classes
             ValueError: When the number of classes entered does not match the expected number
         """
-        
-        if y_pred.shape != y_true.shape:
-            raise ValueError(f"ground truth has different shape ({y_true.shape}) from input ({y_pred.shape})")
 
         if len(y_pred.shape) != 4 and len(y_pred.shape) != 5:
             raise ValueError(f"input shape must be 4 or 5, but got {y_pred.shape}")
         
-        if torch.max(y_true) != self.num_classes - 1:
-            raise ValueError(f"Please make sure the number of classes is {self.num_classes-1}")
+        n_pred_ch = y_pred.shape[1]
+        
+        if self.softmax:
+            if n_pred_ch == 1:
+                raise ValueError("single channel prediction, `softmax=True` ignored.")
+            else:
+                y_pred = torch.softmax(y_pred, 1)
+
+        if self.to_onehot_y:
+            if n_pred_ch == 1:
+                raise ValueError("single channel prediction, `to_onehot_y=True` ignored.")
+            else:
+                y_true = one_hot(y_true, num_classes=n_pred_ch)
 
         asy_focal_loss = self.asy_focal_loss(y_pred, y_true)
         asy_focal_tversky_loss = self.asy_focal_tversky_loss(y_pred, y_true)

src/models/nnUnet/nnUnet_SAM.py
@@ -25,7 +25,7 @@ class SAMMed3D_nnUnet(nn.Module):
         input_channels: int,
         n_stages: int,
         features_per_stage: Union[int, List[int], Tuple[int, ...]],
-        conv_op: Type[_ConvNd],
+        conv_op: Type[_ConvNd] = nn.Conv3d,
         kernel_sizes: Union[int, List[int], Tuple[int, ...]] = 3,
         strides: Union[int, List[int], Tuple[int, ...]] = (1, 2, 2, 2, 2, 2),
         n_conv_per_stage: Union[int, List[int], Tuple[int, ...]] = [2, 2, 2, 2, 2, 2],
@@ -44,7 +44,7 @@ class SAMMed3D_nnUnet(nn.Module):
         # sam_model_type: str = "vit_t",
         sam_image_encoder_ckpt: str = "/home/duong.quang.minh/project/3D-SegClass-Medical/checkpoints/sammed3d/sam_med3d_turbo_image_encoder.pth",
         pretrained: bool = True,
-        trainable_encoder: bool = False,
+        trainable_encoder: bool = True,
     ):
         """
         nonlin_first: if True you get conv -> nonlin -> norm. Else it's conv -> norm -> nonlin

src/utils/callbacks/wandb_callback_lidc_3d.py
@@ -38,7 +38,8 @@ class LIDC_3D_Callback(Callback):
         case_names: tuple[str] = [ "0068", "0027", "0050", "0061", "0074", "0101", "0117"],
         number_of_logged_samples: int = 2,
         do_ds: bool = False,
-        coefficient: int = 5
+        coefficient: int = 5,
+        softmax: bool = False
     ):
         self.num_classes = num_classes
         self.roi_size = roi_size
@@ -46,6 +47,7 @@ class LIDC_3D_Callback(Callback):
         self.classes_name = classes_name
         self.colors = colors
         self.do_ds = do_ds
+        self.softmax = softmax
         
         self.data_nodule_dir = data_nodule_dir
         self.data_clean_dir = data_clean_dir
@@ -94,44 +96,11 @@ class LIDC_3D_Callback(Callback):
                     num_samples=1, # number of cropped samples
                     image_key="image",
                 ),
-                # transforms.Resized(
-                #     keys=["image", "label"],
-                #     spatial_size=self.roi_size,
-                #     mode=("trilinear", "nearest"),  # Image: trilinear, Label: nearest-neighbor
-                # ),
                 transforms.ToTensord(keys=["image", "label"])
             ]
         )
         
     def on_train_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"):
-        # # First sample in case names
-        # image = np.load(self.image_dict[0]["image"])
-        # label = np.load(self.image_dict[0]["label"])
-        
-        # # Clipped
-        # image[image > self.max_px] = self.max_px
-        # image[image < self.min_px] = self.min_px
-        
-        # volume = gif_visualization(
-        #     image=image,
-        #     label=label,
-        #     num_classes=self.num_classes,
-        #     colors=self.colors,
-        #     transparency=0.4
-        # )
-        
-        # frames = [Image.fromarray(volume[i]) for i in range(len(volume))]
-        # gif_buffer = BytesIO()
-        # frames[0].save(gif_buffer, format="GIF", save_all=True, append_images=frames[1:], duration=100, loop=0)
-        # gif_buffer.seek(0)
-        
-        # # trainer.logger.log_image(
-        # #     key=f"Ground truth sample",
-        # #     images=[wandb.Video(gif_buffer, format="gif")],
-        # # )
-        # wandb.log({"Ground truth sample": wandb.Video(gif_buffer, format="gif")})
-        
-        # gif_buffer.close()
         pass
 
     def on_train_epoch_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"):
@@ -146,13 +115,25 @@ class LIDC_3D_Callback(Callback):
             image = image.unsqueeze(0).to(trainer.model.device)
             
             if not self.do_ds:
-                prob = torch.sigmoid(trainer.model(image)) # [1, 1, 128, 128, 128]
+                if not self.softmax:
+                    prob = torch.sigmoid(trainer.model(image)) # [1, 1, 128, 128, 128]
+                    seg = prob[0].detach().cpu().numpy() # [1, 128, 128, 128]
+                    seg = (seg > 0.5).astype(np.int8)
+                else:
+                    prob = trainer.model(image)
+                    prob = torch.softmax(prob, dim=1).detach().cpu().numpy()
+                    prob = np.argmax(prob, axis=1).astype(np.uint8)
+                    seg = prob # [1, 128, 128, 128]
             else:
-                prob = torch.sigmoid(trainer.model(image)[0])
-                
-            seg = prob[0].detach().cpu().numpy() # [1, 128, 128, 128]
-
-            seg = (seg > 0.5).astype(np.int8)
+                if not self.softmax:
+                    prob = torch.sigmoid(trainer.model(image)[0])
+                    seg = prob[0].detach().cpu().numpy() # [1, 128, 128, 128]
+                    seg = (seg > 0.5).astype(np.int8)
+                else:
+                    prob = trainer.model(image)[0] # [1, 2, 128, 128, 128]
+                    prob = torch.softmax(prob, dim=1).detach().cpu().numpy() # [1, 2, 128, 128, 128]                    
+                    prob = np.argmax(prob, axis=1).astype(np.uint8) # [1, 128, 128, 128]
+                    seg = prob
                         
             image = image.squeeze(0)         
             case = self.image_dict[i]["case"]            
@@ -195,10 +176,15 @@ class LIDC_3D_Callback(Callback):
         with torch.no_grad():
             for batch in self.n_samples_validation:
                 image = batch["image"] # [128, 128, 128]
-                label = batch["label"] # [128, 128, 128]                
-                prob = torch.sigmoid(batch["preds"].unsqueeze(0))
-                seg = prob[0].cpu().numpy()
-                seg = (seg > 0.5).astype(np.int8)
+                label = batch["label"] # [128, 128, 128]
+                if not self.softmax:              
+                    prob = torch.sigmoid(batch["preds"].unsqueeze(0))
+                    seg = prob[0].cpu().numpy()
+                    seg = (seg > 0.5).astype(np.int8)
+                else:
+                    prob = torch.softmax(batch["preds"].unsqueeze(0), dim=1).detach().cpu().numpy()                    
+                    prob = np.argmax(prob, axis=1).astype(np.uint8)                    
+                    seg = prob[0] # [1, 128, 128, 128]
                 
                 self.visualization_process(
                     trainer=trainer,
@@ -241,9 +227,14 @@ class LIDC_3D_Callback(Callback):
                 image = batch["image"] # [128, 128, 128]
                 label = batch["label"] # [128, 128, 128]
                                 
-                prob = torch.sigmoid(batch["preds"].unsqueeze(0))
-                seg = prob[0].cpu().numpy()
-                seg = (seg > 0.5).astype(np.int8)
+                if not self.softmax:              
+                    prob = torch.sigmoid(batch["preds"].unsqueeze(0))
+                    seg = prob[0].cpu().numpy()
+                    seg = (seg > 0.5).astype(np.int8)
+                else:
+                    prob = torch.softmax(batch["preds"].unsqueeze(0), dim=1).detach().cpu().numpy()
+                    prob = np.argmax(prob, axis=1).astype(np.uint8)
+                    seg = prob[0] # [1, 128, 128, 128]
                 
                 self.visualization_process(
                     trainer=trainer,