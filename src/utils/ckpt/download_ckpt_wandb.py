52289bc7d7f31d444c0d99cf57cc6092e017b819
minhminh2k <minhqd9112003@gmail.com>
2025-05-03 02:48:29 +0000

    Feat: Upload and download ckpt

---
+++ src/utils/ckpt/download_ckpt_wandb.py
@@ -0,0 +1,18 @@
+import wandb
+
+# Initialize the Weights & Biases API
+api = wandb.Api()
+
+# Get the run
+run_id = "88znpzxh"
+run = api.run(f"minhqd9112003/3d-segmentation/{run_id}")
+
+# File you want to download
+file_path = "logs/train/runs/2023-08-08_17-15-09/checkpoints/epoch_048.ckpt"
+
+# Location to save the downloaded file
+location = "/workspace/ckpt/"
+
+# Download
+run.file(file_path).download(root=location)
+print(f'Successfully downloaded file "{file_path}" to {location}')
--- src/utils/ckpt/upload_ckpt_wandb.py
+++ src/utils/ckpt/upload_ckpt_wandb.py
@@ -4,11 +4,11 @@ import wandb
 api = wandb.Api()
 
 # Get the run
-run_id = "3f9yjeab"
-run = api.run(f"minhqd9112003/medical-image/{run_id}")
+run_id = "88znpzxh"
+run = api.run(f"minhqd9112003/3d-segmentation/{run_id}")
 
 # Location of file you want to upload
-ckpt_path = "/work/hpc/dqm/Lung-Nodule-Segmentation/logs/train/runs/2024-01-10_01-06-52/checkpoints/epoch_134.ckpt"
+ckpt_path = "/home/duong.quang.minh/project/3D-SegClass-Medical/checkpoints/unetr.ckpt"
 
 # Upload
 run.upload_file(ckpt_path)
